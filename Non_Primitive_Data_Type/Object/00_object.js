14338c647147e17370a6b0c41d8618b992c5cc73
ajayshrma <ajaysharma27600@gmail.com>
2024-01-06 21:43:52 +0530

    adding some more questions

---
+++ Non_Primitive_Data_Type/Object/00_object.js
@@ -0,0 +1,53 @@
+/* 
+
+1:Object Creation:
+
+How can you create an empty JavaScript object, and how do you add properties to it?
+
+2:Object Methods:
+
+Explain the difference between the Object.keys(), Object.values(), and Object.entries() methods in JavaScript. Provide examples to illustrate their usage.
+
+
+3:Object Inheritance:
+
+Describe how object inheritance works in JavaScript. How can you create an object that inherits properties and methods from another object? Provide an example to demonstrate inheritance in JavaScript objects. */
+
+
+
+//Empty obj
+
+const myObj= {};  //using object literals
+
+const objconstructor = new obj(); // Using Object constructor
+
+//adding properties to it 
+
+myObj.name = "ajay";
+myObj.age = "24";
+myObj.gender = "Male"
+
+// console.log(myObj);
+
+
+class mainClass {
+  parentObject = {
+    name:"ajay",
+    age: "24",
+    gender: "Male",
+    }
+}
+
+
+class baseClass extends mainClass{
+
+
+};
+
+
+
+
+
+
+
+
