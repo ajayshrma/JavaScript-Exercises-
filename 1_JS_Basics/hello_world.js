663ded160fc7fab331ab290861d57e02bfd81e3e
ajayshrma <ajaysharma27600@gmail.com>
2024-01-08 22:19:16 +0530

    Let & const basics

--- 1_JS_Basics/hello_world.js
+++ 1_JS_Basics/hello_world.js
@@ -7,15 +7,25 @@
 // // Usage:
 // console.log(createHelloWorld()); 
 
-let v = 4;
+// let v = 4;
 
 
-function counter(v)
-{
-return function(){
-    return counter++    ;
-}
-};
+// function counter(v)
+// {
+// return function(){
+//     return counter++    ;
+// }
+// };
 
-console.log(counter());
+// console.log(counter());
 
+//we can redeclare the num value
+let num  =6;
+num = 4;
+console.log(num);  //output 4
+
+//we can't redeclare num2 using const
+
+const num2 = 20;
+num2 = 30;
+console.log(num2);  ///output : TypeError: Assignment to constant variable.
